ba219cd9ba8c889973eac0ef1dab27513327033d
Jonas <jonasg@umich.edu>
2025-03-17 14:20:42 -0400

    Rename to GetHub lmao

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header class="flex w-full justify-center items-center text-center max-screen-w-md mx-auto h-14 min-w-[320px] font-semibold">
       <a class="text-gray-100" href="/">
-          quickcopy
+          commandc
       </a>
     </header>
   );
