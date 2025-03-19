b1212f376c28d1046b39762af49978b61de174a1
Jonas <jonasg@umich.edu>
2025-03-19 13:31:35 -0400

    Add copy buttons if supported

--- islands/Get.tsx
+++ islands/Get.tsx
@@ -8,6 +8,8 @@ export function Get() {
   const [fileType, setFileType] = useState<string | null>(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [messageCopied, setMessageCopied] = useState(false);
+  const [fileCopied, setFileCopied] = useState(false);
 
   const handleSubmit = async () => {
     setLoading(true);
@@ -48,6 +50,27 @@ export function Get() {
     setLoading(false);
   };
 
+  const handleCopyMessage = () => {
+    navigator.clipboard.writeText(message);
+    setMessageCopied(true);
+    setTimeout(() => setMessageCopied(false), 3000);
+  };
+
+  const handleCopyFile = async () => {
+    if (!fileUrl) return;
+    try {
+      const response = await fetch(fileUrl);
+      const blob = await response.blob();
+      await navigator.clipboard.write([
+        new ClipboardItem({ [`web ${fileType!}`]: blob }),
+      ]);
+      setFileCopied(true);
+      setTimeout(() => setFileCopied(false), 3000);
+    } catch (error) {
+      console.error("Failed to copy file:", error);
+    }
+  };
+
   return (
     <div class="flex flex-col items-center text-gray-100 pt-16">
       <h1>get a message</h1>
@@ -67,13 +90,40 @@ export function Get() {
         {loading ? "loading..." : "submit"}
       </button>
       {error && <p class="text-red-500">error: {error}</p>}
-      {message !== "" && <p>message: {message}</p>}
+      {message !== "" && (
+        <>
+          <p>message: {message}</p>
+          <button
+            type="button"
+            class={!messageCopied ? `hover:text-gray-400` : ""}
+            onClick={handleCopyMessage}
+          >
+            {messageCopied ? "copied!" : "copy message"}
+          </button>
+        </>
+      )}
       {fileUrl && (
         <>
           <p>{fileName}</p>
-          <a href={fileUrl} download={fileName || "downloaded_file"}>
-            download file
-          </a>
+          <div class="flex flex-row gap-2 mb-2">
+            <a
+              href={fileUrl}
+              class="hover:text-gray-400"
+              download={fileName || "downloaded_file"}
+            >
+              download file
+            </a>
+            {fileType && ClipboardItem.supports(fileType) &&
+              (
+                <button
+                  type="button"
+                  class={!fileCopied ? `hover:text-gray-400` : ""}
+                  onClick={handleCopyFile}
+                >
+                  {fileCopied ? "copied" : "copy file"}
+                </button>
+              )}
+          </div>
           {fileType && fileType.startsWith("image/") && (
             <img
               src={fileUrl}
